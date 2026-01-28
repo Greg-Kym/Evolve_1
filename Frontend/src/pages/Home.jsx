d56d6c7f06a04b537d50814fa01be2a54d8a92a7
Gregory Kimani <gregkymgreg@gmail.com>
2026-01-28 21:52:39 +0300

    Project 1

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -13,6 +13,9 @@ export default function App() {
       </div>
 
       <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
 
       <div className="main-homepage">
         <p className="welcome">
