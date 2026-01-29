28e90f61e99185171c0bccf09a6b65698ae20801
Gregory Kimani <gregkymgreg@gmail.com>
2026-01-30 00:19:51 +0300

    Project 1

--- Backend/models/user-model.js
+++ Backend/models/user-model.js
@@ -4,6 +4,11 @@ const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  isVerified: { type: Boolean, default: false },
+  verifyOtp: { type: String, default: "" },
+  verifyOtpExpireAt: { type: Number, edfault: 0 },
+  resetOtp: { type: String, default: "" },
+  resetOtpExpireAt: { type: Number, default: 0 },
 });
 
 const UserModel = mongoose.model("User", UserSchema);
