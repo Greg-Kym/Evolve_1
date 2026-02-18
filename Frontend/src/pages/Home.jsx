52c40a0a0a002cd51c9ac04074ab45cace596a15
Gregory Kimani <gregkymgreg@gmail.com>
2026-02-18 23:50:25 +0300

    Project Evo

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -18,10 +18,6 @@ export default function App() {
 
       <div></div>
       <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
 
       <div className="main-homepage">
         <p className="welcome">
